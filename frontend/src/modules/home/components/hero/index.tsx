105fd73505e1547be1a467eb086a477ef20fab52
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2022-10-01 18:04:22 -0400

    modified site slightly more towards our goals

--- frontend/src/modules/home/components/hero/index.tsx
+++ frontend/src/modules/home/components/hero/index.tsx
@@ -6,11 +6,10 @@ const Hero = () => {
     <div className="h-[90vh] w-full relative">
       <div className="text-white absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:text-left small:justify-end small:items-start small:p-32">
         <h1 className="text-2xl-semi mb-4 drop-shadow-md shadow-black">
-          Summer styles are finally here
+          More numbers are finally here!
         </h1>
         <p className="text-base-regular max-w-[32rem] mb-6 drop-shadow-md shadow-black">
-          This year, our new summer collection will shelter you from the harsh
-          elements of a world that doesn&apos;t care if you live or die.
+          Lol we sell primes
         </p>
         <UnderlineLink href="/store">Explore products</UnderlineLink>
       </div>
